38c2c5dcfa39e6f735e97b9b74cc2d2cd6633fea
cboscariol <caamila.boscariol@gmail.com>
2021-10-11 20:11:24 -0300

    music list

---
+++ desafio-mesha/src/Components/CheckListModal/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function ModalList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ModalList
--- desafio-mesha/src/Components/MusicList/index.js
+++ desafio-mesha/src/Components/MusicList/index.js
@@ -2,8 +2,16 @@ import './style.css'
 
 function MusicList() {
   return (
-    <div>
-      
+    <div className='container-music-list'>
+      <h3 className='h3-music-list'>Lista de músicas sugeridas a partir da temperatura</h3>
+      <ul>
+        <li className='music-list'>
+          <p >
+            music track
+          </p>
+        </li>
+      </ul>
+      <button className='btn-save-musics'>Salvar lista</button>
     </div>
   )
 }
